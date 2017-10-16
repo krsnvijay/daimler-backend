de4b4037bfdbaf2eec0272b62b20e2a2acd536f6
krsnvijay <krsnvijay@gmail.com>
2017-10-16 18:55:33 +0530

    Simple Excel Parse for Django Admin

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from import_export.admin import ImportExportModelAdmin
+
+from app.models import PartResource, Part
+
+@admin.register(Part)
+class PartAdmin(ImportExportModelAdmin):
+    resource_class = PartResource
+    search_fields = ['part_number', 'supplier_name','truck_details']
+    list_filter = ('supplier_name', 'truck_details')
+    list_display = ('part_number', 'description', 'supplier_name', 'eta_dicv','truck_details','shortage_reason')
--- daimler_backend/settings.py
+++ daimler_backend/settings.py
@@ -17,7 +17,6 @@ import dj_database_url
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
@@ -28,7 +27,6 @@ SECRET_KEY = 'wb7+sv28mjpbcw97)0mz8809$9tapb56^_y-26*gxy(my&r!2f'
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
-
 
 # Application definition
 
@@ -44,6 +42,7 @@ INSTALLED_APPS = [
     'sos.apps.SosConfig',
     'vehicle_loss.apps.VehiclelossConfig',
     'rest_framework',
+    'import_export',
 ]
 
 MIDDLEWARE = [
@@ -78,7 +77,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'daimler_backend.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
@@ -88,7 +86,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
@@ -108,7 +105,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
@@ -121,7 +117,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
