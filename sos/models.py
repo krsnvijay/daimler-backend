c00476b1f45a10c53220e54f9ef3232eddccde1b
Sudarshan <sudarshan97@gmail.com>
2017-10-29 18:36:54 +0530

    Added sos models

--- sos/models.py
+++ sos/models.py
@@ -1,3 +1,26 @@
+import datetime
+from django.urls import reverse
 from django.db import models
 
 # Create your models here.
+
+
+class Sos(models.Model):
+    class Meta:
+        ordering= ["-id"]
+    date= models.DateField(max_length=30,help_text="Date",default=datetime.now)
+    id= models.CharField(max_length=30,help_text="Id")
+    uid= models.CharField(max_length=30,help_text="Uid")
+    name= models.CharField(max_length=50,help_text="Name")
+    level=models.IntegerField(max_length=2,help_text="Enter Level")
+    content=models.CharField(max_length=100,help_text="Enter Content")
+    media= models.CharField(max_length=20,help_text="Media")
+
+    def __str__(self):
+        return self.id
+
+    def get_absolute_url(self):
+        """
+        Returns the url to access a particular instance of MyModelName.
+        """
+        return reverse('model-detail-view', args=[str(self.id)])
